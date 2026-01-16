3394de619c3ad209efab7965568df08daeb67e17
Skyl3Z <sabagogichaishvili15@gmail.com>
2026-01-16 04:22:20 +0400

    Update project files

---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tictactoe_console_game
+{
+    internal class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("name should contain at least 1 character");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
+        }
+
+        public int winCount;
+    }
+}
